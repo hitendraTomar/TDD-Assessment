3e18f4ee4f5e68647eead004e7915c85e4206a45
Hitendra <tomhitt771@gmail.com>
2024-03-04 17:38:08 +0530

    Generated TDD for string calculator

---
+++ spec/calculator_spec.rb
@@ -0,0 +1,51 @@
+# TDD without monkey patch for calculator_spec.rb
+
+RSpec.describe Calculator do
+  describe '#add_numbers' do
+    before do
+      @calculator = Calculator.new
+    end
+
+    context 'when input is an empty string' do
+      it 'returns 0' do
+        expect(@calculator.add_numbers("")).to eq(0)
+      end
+    end
+
+    context 'when input is contains a single number' do
+      it 'returns the number' do
+        expect(@calculator.add_numbers("1")).to eq(1)
+      end
+    end
+
+    context 'when input contains multiple numbers separated by commas' do
+      it 'returns the sum of the numbers' do
+        expect(@calculator.add_numbers("1,5,6")).to eq(12)
+      end
+    end
+
+    context 'when string contains negative numbers' do
+      it 'raises an exception with negative numbers' do
+        expect { @calculator.add_numbers("1,-2,3,-4") }.to raise_error(StandardError, "negative numbers not allowed: -2,-4")
+      end
+    end
+
+    context 'when input contains a custom delimiter' do
+      it 'returns the sum of the numbers include in the string' do
+        expect(@calculator.add_numbers("//;\n1;2;3")).to eq(6)
+      end
+    end
+
+    context 'when string contains numbers separated by spaces' do
+      it 'returns the sum of the numbers' do
+        expect(@calculator.add_numbers("1, 5")).to eq(6)
+      end
+    end
+
+    context 'when input contains new line at the end' do
+      it 'raises an exception with invalid input' do
+        expect { @calculator.add_numbers("1,\n") }.to raise_error(StandardError, "invalid input")
+      end
+    end
+  end
+end
---
+++ spec/string_spec.rb
@@ -0,0 +1,47 @@
+# TDD using monkey patch for string_spec.rb
+
+RSpec.describe String do
+  describe '#add_numbers' do
+    context 'when input is an empty string' do
+      it 'returns 0' do
+        expect("".add_numbers).to eq(0)
+      end
+    end
+
+    context 'when input contains a single number' do
+      it 'returns the number' do
+        expect("1".add_numbers).to eq(1)
+      end
+    end
+
+    context 'when input contains multiple numbers separated by commas' do
+      it 'returns the sum of the numbers' do
+        expect("1,2,3,4,5".add_numbers).to eq(15)
+      end
+    end
+
+    context 'when input contains new lines between numbers' do
+      it 'returns the sum of the numbers' do
+        expect("1\n2,3".add_numbers).to eq(6)
+      end
+    end
+
+    context 'when input contains new lines at the end' do
+      it 'raises an exception with invalid input' do
+        expect { "1,\n".add_numbers }.to raise_error(StandardError, "invalid input")
+      end
+    end
+
+    context 'when input contains a custom delimiter' do
+      it 'returns the sum of the numbers' do
+        expect("//;\n1;2;3".add_numbers).to eq(6)
+      end
+    end
+
+    context 'when input contains negative numbers' do
+      it 'raises an exception with negative numbers' do
+        expect { "1,-2,3,-4".add_numbers }.to raise_error(StandardError, "negative numbers not allowed: -2,-4")
+      end
+    end
+  end
+end
